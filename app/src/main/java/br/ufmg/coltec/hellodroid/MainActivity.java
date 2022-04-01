1ccef860f0f76f63f150000d78cbe0222d6f12b3
pogum <samuel.pogum@gmail.com>
2022-03-31 21:24:47 -0300

    Android E01: Hello, USER! - Trabalhando com logging

--- app/src/main/java/br/ufmg/coltec/hellodroid/MainActivity.java
+++ app/src/main/java/br/ufmg/coltec/hellodroid/MainActivity.java
@@ -7,13 +7,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG=MainActivity.class.getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d(TAG,"App inicializado!");
 
         final TextView lblHellodroid=findViewById(R.id.hello_droid);
         final EditText txtName= findViewById(R.id.text_name);
@@ -24,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 String nome=txtName.getText().toString();
                 if(nome.equals("")){
+                    Log.e(TAG,"Campo vazio!");
                     lblHellodroid.setText("Digite seu nome!");
                 }else{
                     lblHellodroid.setText("Hello, "+txtName.getText());
