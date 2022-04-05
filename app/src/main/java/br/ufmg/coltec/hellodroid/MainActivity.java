2d67ee4656e1d18e8d3239ac037a5af6d3689cb6
rafaalt <rafaaslat@gmail.com>
2022-04-05 01:23:29 -0300

    Fim da atividade

--- app/src/main/java/br/ufmg/coltec/hellodroid/MainActivity.java
+++ app/src/main/java/br/ufmg/coltec/hellodroid/MainActivity.java
@@ -19,31 +19,16 @@ public class MainActivity extends AppCompatActivity {
         Button but1 = findViewById(R.id.botao);
         EditText txt = findViewById(R.id.txtNome);
 
-        but1.setOnClickListener((view) ->{
+        but1.setOnClickListener((view) -> {
             String str = "Olá, " + txt.getText().toString();
-            if(str.equals("Olá, ")){
+            if (str.equals("Olá, ")) {
                 Toast.makeText(this, "Campo vazio", Toast.LENGTH_LONG).show();
-                Log.e("MainActivity","Campo vazio.");
-            }
-            else if(str.equals("Olá, Digite seu nome aqui"))
+                Log.e("MainActivity", "Campo vazio.");
+            } else if (str.equals("Olá, Digite seu nome aqui"))
                 Toast.makeText(this, "Você não alterou o seu nome.", Toast.LENGTH_LONG).show();
 
             else
                 Toast.makeText(this, str, Toast.LENGTH_SHORT).show();
         });
     }
-}/***Bateria**
- Para realizar o monitoramento da bateria através do Android Studio utiliza-se a classe
- Intent que recebe todos os dados possíveis pela classe BatteryManager, é possível verificar se a bateria
- está sendo carregada no momento pela função *batteryStatus.getIntExtra(BatteryManager.EXTRA_STATUS, -1)* e até o
- modo pelo qual está sendo carregada pela função *batteryStatus.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1)*
-
- **GPS**
- Para ter o controle do GPS durante o uso do aplicativo, primeiro, é necessário pedir a permissão do usuario para
- ativar a localização do dispositivo com o aplicativo pelo manifesto *<uses-permission android:name="android.permission.ACCESS_BACKGROUND_LOCATION"*
- tendo vários níveis de acesso a localização, sendo aproximada ou exata, e durante todo o uso do aplicativo ou somente durante sua execução.
-
- **Ligacao**
-
-
- */
+}
--- app/src/main/res/guia-emulador.md
+++ app/src/main/res/guia-emulador.md
@@ -8,9 +8,10 @@ modo pelo qual está sendo carregada pela função *batteryStatus.getIntExtra(Ba
 Para ter o controle do GPS durante o uso do aplicativo, primeiro, é necessário pedir a permissão do usuario para
 ativar a localização do dispositivo com o aplicativo pelo manifesto *<uses-permission android:name="android.permission.ACCESS_BACKGROUND_LOCATION"*
 tendo vários níveis de acesso a localização, sendo aproximada ou exata, e durante todo o uso do aplicativo ou somente durante sua execução.
+Utilizando as funções *getLastLocation()* e *getCurrentLocation()* é possível obter os dados do aparelho e utilizá-los no app.
 
 **Ligacao**
-Para efetuar uma ligação também é necessário a permissão do usuário que é dada pelo manifesto *<uses-permission android:name="android.permission.CALL_PHONE" />*
+Para efetuar uma ligação também é necessário a permissão do usuário que é dada pelo manifesto *<uses-permission android:name="android.permission.CALL_PHONE"/>*
 Para conseguir fazer uma ligacao pelo aplicativo é utilizado a classe Intent com as funções 
 *Uri uri = Uri.parse("tel:"+telefone);
 Intent intent = new Intent(Intent.ACTION_DIAL,uri);
